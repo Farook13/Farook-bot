8418ff9b906333e1a19b63a62062b645c45d5c8b
Farook13 <umarulfarook787@gmail.com>
2025-02-23 17:44:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 # Telegram API credentials
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-MONGO_URI = os.getenv('MONGO_URI')
+API_ID = os.getenv('12618934')
+API_HASH = os.getenv('49aacd0bc2f8924add29fb02e20c8a16')
+BOT_TOKEN = os.getenv('7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
+MONGO_URI = os.getenv('mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 OMDB_API_KEY = os.getenv('OMDB_API_KEY')
 
 # Check for missing credentials
