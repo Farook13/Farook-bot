cefbd48fa063e97b1172898c42ed966c0df63804
Farook13 <umarulfarook787@gmail.com>
2025-02-23 15:50:46 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from bot import app
+
+# Load environment variables
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Bot is running...")
+    app.run()
+​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
